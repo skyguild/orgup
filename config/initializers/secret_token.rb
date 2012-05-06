a33d167370e829323fc6f818201412500d1acb03
ErinaMikami <erina.m29@gmail.com>
2012-05-07 05:24:55 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orgup::Application.config.secret_token = '06aa09dfa9b87fdc74061ecaae3e5388baacf8f5abec66344e310e16de2aaf461ef4b144780a20ec5a1efaa88217773357ab3c161b48120764ffbefccd701dbb'
